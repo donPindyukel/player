f9014c2c7808474803d0db4172bda600598ea76a
donPindykel <a913000@gmail.com>
2015-11-10 13:12:43 +0600

    Готовый вариант

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,24 @@
 
+$(document).ready(function(){
+
+	$("#check").change(function(event){
+		var check = event.target.checked;
+
+		if(check){
+            
+            $("#watchBtn").css({"background": "url(../img/play.png) center no-repeat", 
+            	                "background-size":"contain",
+            	                 "cursor":"pointer"});
+
+		}
+
+		else {
+			$("#watchBtn").css({"background":"url(../img/play-blackwhite.png) center no-repeat",
+				                "background-size":"contain",
+				                 "cursor":"default"});
+		}
+		
+	});
+
+
+});
